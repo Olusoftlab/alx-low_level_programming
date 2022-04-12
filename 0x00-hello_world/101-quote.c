78bf1e6f1d25c44f75f2a50e17084d6452bd3309
olumild <olumild998@gmail.com>
2022-04-12 12:55:26 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main -entry point
- *
- * return: 0 success
+ * main - entry point
+ * Return: 1
  */
-int (void)
+int main(void)
 {
-	write(1,"and that piece of artis useful\", - Dora korpar, 2015-10-19\n",59);
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 	return (1);
-}
-
