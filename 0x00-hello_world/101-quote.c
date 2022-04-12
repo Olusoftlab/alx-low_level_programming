73bacbe7d8a5e295a10e54168aa64e4ffed97052
olumild <olumild998@gmail.com>
2022-04-12 07:19:01 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * return: 0 always success
+ */
+int main(void)
+{
+	write(1,"and that piece of art is useful"  - Dora korpar, 2015-10-19 ," 59);
+	return (1);
+}
