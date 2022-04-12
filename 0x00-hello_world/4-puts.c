f0fb403fe71cc5e0fa011b50fa9344c9caee093d
olumild <olumild998@gmail.com>
2022-04-12 01:34:09 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * return 0 after printing the function
  */
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzlen");
 return (0);
 }
 
