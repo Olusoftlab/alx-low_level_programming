bf0348345a3fbc9e128ddd17ea9814bbf72c2c04
olumild <olumild998@gmail.com>
2022-04-16 21:45:23 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int digit_numbers;
+
+for (digit_numbers = 0; digit_numbers <= 9;; digit_numbers++)
+{
+	printf(digit_numbers);
+}
+
+printf('\n');
+
+return (0);
+}
