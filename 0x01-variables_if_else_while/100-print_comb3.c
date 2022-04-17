2efe499bd6e2581dc56775cbbceee387f5563630
olumild <olumild998@gmail.com>
2022-04-17 11:43:48 -0700

    task11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+for (num1 = 48; num1 < 57; num1++)
+{
+	for (num2 = 49; num2 <= 57; num2++)
+	{
+		putchar(num1);
+		putchar(num2);
+
+		if (num1 == 56 &&  num2 == 57)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+}
+
+putchar('\n');
+
+return (0);
+}
