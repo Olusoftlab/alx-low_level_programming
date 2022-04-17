a7b83c01e7bb00caf370855194511cd3e78b0844
olumild <olumild998@gmail.com>
2022-04-17 05:05:16 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char hexa;
+
+for (hexa = 48; hexa <= 57; hexa++)
+{
+	putchar(hexa);
+}
+
+
+for (hexa = 97; hexa <= 102; hexa++)
+{
+	putchar(hexa);
+}
+
+putchar('\n');
+
+return (0);
+}
