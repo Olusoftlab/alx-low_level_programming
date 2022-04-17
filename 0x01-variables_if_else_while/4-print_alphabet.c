3f7f00090f4d91276d033da05c4da6457bdeb951
olumild <olumild998@gmail.com>
2022-04-16 21:24:06 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char alpha_lowercase;
+
+for (alpha_lowercase = 'a'; alpha_lowercase <= 'z'; alpha_lowercase++)
+{
+	if (alpha_lowercase = 'e')
+		continue;
+
+
+	if (alpha_lowercase = 'q')
+		continue;
+
+	putchar(alpha_lowercase);
+}
+
+putchar('\n');
+
+return (0);
+}
+
+
+
