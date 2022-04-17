4919a6a23b91972ab49c317686004eb1a9cfe8d5
olumild <olumild998@gmail.com>
2022-04-17 04:08:48 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int digitz;
+
+for (digitz = 48; digitz <= 57; digitz++)
+{
+	putchar(digitz);
+}
+
+putchar('\n');
+
+return (0);
+}
