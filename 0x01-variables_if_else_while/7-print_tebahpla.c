d9c8a5a3f9072ba388908448ca1a7cb3f67e2e67
olumild <olumild998@gmail.com>
2022-04-17 04:28:05 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char reverse;
+
+for (reverse = 'z'; reverse <= 'a'; reverse--)
+{
+	putchar(reverse);
+}
+
+putchar('\n');
+
+return (0);
+}
