d1fb31bc6899df71d3d0fcb7db2c4ea4c4a0586a
olumild <olumild998@gmail.com>
2022-04-18 01:10:52 -0700

    task11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int num1;
 	int num2;
 
-for (num1 = 48; num1 < 57; num1++)
+for (num1 = 48; num1 < 57; num1 = num1 + 1)
 {
-	for (num2 = num1+1; num2 <= 57; num2++)
+	for (num2 = num1+1; num2 <= 57; num2 = num2 + 1)
 	{
 		putchar(num1);
 		putchar(num2);
