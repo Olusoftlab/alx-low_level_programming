11aafec7e5b3df005697a98c86869a1be25de412
olumild <olumild998@gmail.com>
2022-04-21 15:29:19 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main entry point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+
+_putchar('\n');
+return(0);
+}
