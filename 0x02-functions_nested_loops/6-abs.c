1f2218a26afbc84a4d6fe11474e25609e4659952
olumild <olumild998@gmail.com>
2022-04-22 07:54:27 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of number
+ * @n: number being tested
+ * Return: 0 always
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+}
