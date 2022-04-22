9e0936784314e24650431c91ba557c768df7b947
olumild <olumild998@gmail.com>
2022-04-22 07:31:24 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main"
+/**
+ * print_sign - print sign of numbeers
+ * @n: numbers being tested
+ * Return: always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+
+	return (0);
+}
+
+
