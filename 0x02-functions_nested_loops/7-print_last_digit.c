e3d3f04169121bd031c1c2349d73549db62337b2
olumild <olumild998@gmail.com>
2022-04-22 08:29:15 -0700

    task6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: number being tested
+ * Return: 0 always
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar(n + '0');
+	}
+	
+	return (0);
+}
