198c94f6651c29c20a589584fdf6e29c34158171
olumild <olumild998@gmail.com>
2022-04-23 05:38:57 -0700

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * print_to_98 - print natural numbers from n to 98
  * @n: tested number
@@ -23,6 +24,3 @@ int print_to_98(int n)
 	}
 	printf("98\n");
 }
-
-
-
