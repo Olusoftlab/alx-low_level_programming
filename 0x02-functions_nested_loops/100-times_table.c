44cee5d66d8c11b9060ccc65ca4d1d7c36184196
olumild <olumild998@gmail.com>
2022-04-24 10:27:39 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,37 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int res;
+	int i, j, res;
 
-	if (!(n > 15 || n <0))
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
-			res = (i * j);
-
-			if (j != 0)
+			for (j = 0; j <= 0; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
 			}
-			if (res < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((res % 10) + '0');
-			}
-			else if (res >= 10 && res < 100)
-			{
-				_putchar(' ');
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if (res >= 100 && j != 0)
-			{
-				_putchar((res / 100) + '0');
-				_putchar((res / 10) % 10 + '0');
-				_putchar((res % 10) + '0');
-			}
-			else
-				_putchar((res % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
 
-
-				
