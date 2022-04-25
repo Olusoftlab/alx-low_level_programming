fef2e7f2d18befa5fe3d48614909a3b205bc7b38
olumild <olumild998@gmail.com>
2022-04-24 20:14:18 -0700

    task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n;  j++)
+			for (j = 0; j <= n; j++)
 			{
 				res = (i * j);
 				if (j != 0)
@@ -25,6 +25,12 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
 				}
 				else if (res >= 100 && j != 0)
@@ -41,3 +47,6 @@ void print_times_table(int n)
 	}
 }
 
+
+
+					
